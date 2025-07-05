ba4640782362e10644790d1a551caefd0527a5c7
Jephte Dufresne <jephtedufo@gmail.com>
2025-07-05 18:11:59 +0300

    Add files via upload

--- backend/next.config.js
+++ backend/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client'],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   async headers() {
     return [
       {
